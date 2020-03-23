5dadebde6ae0d8b6841575481e066d64a2cc608f
Alex Luna <14889546+LeTarrask@users.noreply.github.com>
2020-03-23 14:22:35 +0000

    initial model for pomodoro working

---
+++ Models/PomoModel.swift
@@ -0,0 +1,65 @@
+//
+//  Timer.swift
+//  Pomodoro
+//
+//  Created by Alex Luna on 23/03/2020.
+//  Copyright © 2020 Garagem Infinita. All rights reserved.
+//
+
+import Foundation
+
+class PomoModel: ObservableObject {
+    var pomosCompleted: Int = 0
+    var isResting = false
+    
+    var timer: Timer?
+    
+    func startPomo() {
+        var counter = Counter.working
+
+        switch isResting {
+        case false:
+            counter = Counter.working
+        case true:
+            counter = Counter.resting
+        }
+        
+        timer = Timer.scheduledTimer(timeInterval: counter.rawValue,
+                                     target: self,
+                                     selector: #selector(endPomo),
+                                     userInfo: nil,
+                                     repeats: true)
+    }
+    
+    @objc func endPomo() {
+        print("Pomo ended")
+        
+        if !isResting {
+            pomosCompleted += 1
+            isResting.toggle()
+            startPomo()
+        }
+    }
+}
+
+enum Counter: Double {
+    case working = 1500.0
+    case resting = 300.0
+}
+
+
+extension Double {// THIS CODE CAME FROM OTHER APP. DO WE NEED IT?
+    var seconds: Double { return self }
+
+    var minutes: Double { return self.seconds * 60 }
+
+    var hours: Double { return self.minutes * 60 }
+
+    var days: Double { return self.hours * 24 }
+
+    var weeks: Double { return self.days * 7 }
+
+    var months: Double { return self.weeks * 4 }
+
+    var years: Double { return self.months * 12 }
+}
