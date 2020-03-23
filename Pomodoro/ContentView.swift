b21fb7a472bbbc8494ad31ba63c5f8c9981b65e7
Alex Luna <14889546+LeTarrask@users.noreply.github.com>
2020-03-23 13:28:13 +0000

    Initial Commit

---
+++ Pomodoro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pomodoro
+//
+//  Created by Alex Luna on 23/03/2020.
+//  Copyright © 2020 Garagem Infinita. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
